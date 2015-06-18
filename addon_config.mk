d8e882b17aa2aa608598eb3393858ad149c718fe
Christopher Baker <me@christopherbaker.net>
2015-06-18 16:38:46 -0500

    Add description.

---
+++ addon_config.mk
@@ -0,0 +1,9 @@
+meta:
+	ADDON_NAME = ofxMaps
+	ADDON_DESCRIPTION = A tile-based mapping addon.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "web" "client" "server" "map" "gis"
+	ADDON_URL = http://github.com/bakercp/ofxMaps
+
+common:
+	ADDON_DEPENDENCIES = ofxHTTP ofxGeo
